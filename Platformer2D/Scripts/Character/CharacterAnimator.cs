d4f3e23945659dd68dee55b1a0cd9c04878eed91
VersAndromede <102908677+VersAndromede@users.noreply.github.com>
2022-10-03 16:37:21 +0300

    Update CharacterAnimator.cs

--- Platformer2D/Scripts/Character/CharacterAnimator.cs
+++ Platformer2D/Scripts/Character/CharacterAnimator.cs
@@ -24,21 +24,25 @@ public class CharacterAnimator : MonoBehaviour
 
     private void PlayAnimate()
     {
+        const string IdleAnimation = "Idle";
+        const string RunAnimation = "Run";
+        const string JumpAnimation = "Jump";
+
         if (_character.CharacterState == CharacterState.Idle)
         {
-            _animator.SetBool("Idle", true);
-            _animator.SetBool("Run", false);
+            _animator.SetBool(IdleAnimation, true);
+            _animator.SetBool(RunAnimation, false);
         }
         else if (_character.CharacterState == CharacterState.Run)
         {
-            _animator.SetBool("Run", true);
-            _animator.SetBool("Idle", false);
+            _animator.SetBool(RunAnimation, true);
+            _animator.SetBool(IdleAnimation, false);
         }
         else if (_character.CharacterState == CharacterState.Jump)
         {
-            _animator.SetBool("Run", false);
-            _animator.SetBool("Idle", false);
-            _animator.SetTrigger("Jump");
+            _animator.SetBool(RunAnimation, false);
+            _animator.SetBool(IdleAnimation, false);
+            _animator.SetTrigger(JumpAnimation);
         }
     }
 }
